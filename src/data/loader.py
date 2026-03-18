07bad1b3cfe71a5b8e578b901bbe2c420de6663d
YusrahS <112873830+YusrahS@users.noreply.github.com>
2026-03-18 20:25:06 +0000

    Update loader.py
    updated functions for english, spanish and bilingual corpus

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,13 +1,16 @@
 """
 Date: 11/03/2026
 Dataset loading functions
+
+Date: 18/03/2026
+modified function to call final datasets
 """
 
 import pandas as pd
 import os
 
 #loading the bilingual dataset
-def load_bilingual_dataset(path='Datasets/processed/bilingual_merged.csv'):
+def load_bilingual_dataset(path='Datasets/processed/Bilingual_corpus.csv'):
     if os.path.exists(path):
         df = pd.read_csv(path)
         return df
@@ -15,7 +18,7 @@ def load_bilingual_dataset(path='Datasets/processed/bilingual_merged.csv'):
         print(f" File not found: {path}")
         return None
 
-def load_combined_spanish_dataset(path='Datasets/processed/spanish_merged.csv'):
+def load_offendes_colombian_dataset(path='Datasets/processed/offendes_colombia.csv'):
     if os.path.exists(path):
         df = pd.read_csv(path)
         return df
@@ -39,7 +42,7 @@ def load_colombian_dataset(data_path='Datasets/processed/colombian_dataset_refor
         print(f" File not found: {data_path}")
         return None
 
-def load_english_dataset(data_path='Datasets/processed/english_dataset_reformatted.csv'):
+def load_spanish_corpus(data_path='Datasets/processed/spanish_corpus.csv'):
     if os.path.exists(data_path):
         df = pd.read_csv(data_path)
         return df
